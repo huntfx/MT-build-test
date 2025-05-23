4e6a4870e5561bed3414d9bf60027699fa1e7207
Peter Hunt <peter@huntfx.uk>
2025-05-23 23:29:34 +0100

    test
    Created new drawing system for viewing mouse / gamepad data.
    # Enhancements
    - Mouse render option
    - Gamepad render option
    # Fixes
    - Fixed potential crash when thing happens (550cd1de0cbf36cb02f1f9bb986757febe731fdc)

--- mousetracks2/version.py
+++ mousetracks2/version.py
@@ -1,11 +1,20 @@
-"""
-Defines the application version.
+"""Define the application version.
+This is used for both the application and build process.
 
 IMPORTANT: Modifying the `__version__` string below and committing this
 file to the main branch will automatically trigger a new software release.
 This process includes:
-1. Creating a Git tag named `v<__version__>` (e.g., v1.0.1).
+1. Creating a Git tag named `v<__version__>` (eg. v1.0.1).
 2. Publishing a new GitHub Release with this tag.
 3. The release notes will be based on the commit message of this change.
+4. An executable will be built and attached to the release.
+
+The recommend commit message format is as follows:
+    Created new option.
+    # Enhancements
+    - New option
+    # Fixes
+    - Fixed potential crash when thing happens (<commit_sha>)
 """
+
 __version__ = '2.0.26'
