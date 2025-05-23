a797e23937c7c88915132877c97ae0565cc0954c
Peter Hunt <peter@huntfx.uk>
2025-05-23 09:53:18 +0100

    Release on version update
    Test comment.

---
+++ mousetracks2/version.py
@@ -0,0 +1,11 @@
+"""
+Defines the application version.
+
+IMPORTANT: Modifying the `__version__` string below and committing this
+file to the main branch will automatically trigger a new software release.
+This process includes:
+1. Creating a Git tag named `v<__version__>` (e.g., v1.0.1).
+2. Publishing a new GitHub Release with this tag.
+3. The release notes will be based on the commit message of this change.
+"""
+__version__ = '2.0.6'
